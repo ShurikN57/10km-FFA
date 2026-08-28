f1fc2b450986332f23b2dde4ffdaa12cc31a3553
ShurikN57 <florian.machouche@yahoo.fr>
2026-08-28 11:59:19 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='comparateur-10k-v3-4';
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())await caches.delete(k);await self.clients.claim()})())});
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
